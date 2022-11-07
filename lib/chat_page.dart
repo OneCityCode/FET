bb84800e7875d923f9a319bdb2b51cffb28d2d31
OneCityCode <117225309+OneCityCode@users.noreply.github.com>
2022-11-07 07:45:58 -0500

    11/7 End of Lesson 3

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -25,14 +25,15 @@ class ChatPage extends StatelessWidget {
       body: Column(
         children: [
            Expanded(
-             child: ListView(
-               children: [
-                ChatBubble(alignment: Alignment.centerLeft,
-                    message: "hello"),
-                 ChatBubble(alignment: Alignment.centerRight,
-                     message: "hello"),
-               ],
-             ),
+             child: ListView.builder(
+                 itemCount: 10,
+                 itemBuilder: (context, index){
+               return ChatBubble(
+                   alignment: index % 2 == 0
+                       ? Alignment.centerLeft
+                       : Alignment.centerRight,
+                   message: "hello");
+             })
            ),
           ChatInput(),
         ],
